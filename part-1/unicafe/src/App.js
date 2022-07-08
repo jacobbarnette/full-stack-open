1271b85916a7bda1123dafabd59ddbf5c108d245
jacobbarnette <jacobcbarnette@gmail.com>
2022-07-08 17:37:50 -0400

    solutions for exercise 1.6 & 1.7

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -1,8 +1,38 @@
 import { useState }from 'react'
+import Button from './Button'
 
 const App = () => {
+  const [good, setGood] = useState(6)
+  const [neutral, setNeutral ] = useState(10)
+  const [bad, setBad] = useState(2)
+
+  const addGood = () => {
+    setGood(good + 1)
+  }
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const addBad = () => {
+    setBad(bad + 1)
+  }
+
   return(
-    <div>Hello</div>
+    <div>
+      <div>
+        <h1>Give Feedback</h1>
+        <Button onClick={addGood} text='good'/>
+        <Button onClick={addNeutral} text='neutral'/>
+        <Button onClick={addBad} text='bad'/>
+      </div>
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    </div>
   )
 }
 

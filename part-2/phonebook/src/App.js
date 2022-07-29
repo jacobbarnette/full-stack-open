36bd1acd3d8c606a29a9d06ac481076fc9e97903
jacobbarnette <jacobcbarnette@gmail.com>
2022-07-29 14:48:06 -0400

    completed exercise 2.18-2.19

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -3,61 +3,98 @@ import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personservice from './components/services/personservice'
+const baseUrl = 'http://localhost:3001/persons'
 
-let testCondition = true
+
 const App = () => {
   const [persons, setPersons] = useState([])
-
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
+    axios
+      .get(baseUrl)
           .then(response => {setPersons(response.data)})
   }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    containsObject()
-   
+    //containsObject()
+   if (newName === '' || newNumber === ''){
+     alert('Please fill in the required information')
+     
+   }
+  
+   const existing = persons.find( (person) => 
+     person.name.toLowerCase() === newName.toLowerCase()
+     
+   ) 
+   if(existing && existing.number === newNumber){
+    alert(`${newName} is already added`)
+    setNewName('')
+    setNewNumber('')
+  } else if(existing && existing.number != newNumber){
+    if(
+      window.confirm(`${existing.name} is in the phonebook, would you like to change the number`)
+    ){
+      const updatedPerson = {...existing, number: newNumber}
+      const id = existing.id
+
+      axios
+        .put(id, updatedPerson)
+        .then(response => {
+          console.log(response)
+          setPersons(
+            persons.map(person => (person.id !== id ? person : response))
+          )})
+          return
+    }
+  } else {
     const personObject = {
       name: newName,
       number: newNumber
     }
     axios
-      .post('http://localhost:3001/persons', personObject)
-      .then(response => console.log(response))
-    if(testCondition){
+      .post(`${baseUrl}/persons`, personObject)
       setPersons(persons.concat(personObject))
       setNewName('')
       setNewNumber('')
-    }
   }
+  
+
+    
+  
+    /*const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    persons.map(person => {
+      if(person.name === newName && person.number === newNumber) {
+        alert(`${newName} is already added to phonebook`)
+        return
+      } else if (person.name != newName && person.number != newNumber) {
+        axios
+        .post('http://localhost:3001/persons', personObject)
+            
+      }
+    })*/
+}
+    
+      
   const handleNameChange = (e) => {
-    e.preventDefault()
     setNewName(e.target.value)
   }
 
   const handleNumberChange = (e) => {
-    e.preventDefault()
     setNewNumber(e.target.value)
   }
-  
-  function containsObject(){
-    persons.map(person => {
-      if(person.name === newName) {
-        alert(`${newName} is already added to phonebook`)
-        testCondition = false
-        setNewName('')
-        setNewNumber('')
-      } 
-    })
-  }
-  function handleFilterText(e){
+
+  const handleFilterText = (e) =>  {
     setFilterText(e.target.value)
   }
-  
+
   const renderedPeople = filterText === ''
     ? persons
     : persons.filter(person => 
@@ -73,12 +110,16 @@ const App = () => {
       {renderedPeople.map(person => {
         
         return(
-          
-          <p key={person.number}>{person.name} {person.number}</p>
+          <div>
+             <Persons person={person}/>
+          </div>
+         
         )})}
-      <Persons renderedPeople={renderedPeople}/>
+        
+      
     </div>
   )
 }
+
 
 export default App
--- part-2/phonebook/src/components/Persons.js
+++ part-2/phonebook/src/components/Persons.js
@@ -1,8 +1,18 @@
-const Persons = ({renderedPeople}) => {
-  console.log(renderedPeople)
-  renderedPeople.map(person => {
-  return  <li key={person.number}>lol</li>
-  })
+import axios from 'axios'
+const Persons = ({person}) => {
+  const baseUrl = 'http://localhost:3001/persons'
+  const deleteNumber = () => {
+    axios
+      .delete(`${baseUrl}/${person.id}`)
+      window.confirm('are you sure')
+   }
+    return(
+      <div>
+         <li id={person.id}>{person.name} {person.number}</li>
+         <input type='submit' onClick={deleteNumber}/>
+      </div>
+    )
+  
 }
 
 export default Persons
